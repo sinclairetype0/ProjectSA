92f885ee8082b7a3c196229e10ed4eece5c58b4c
Raynaldi Susanto <39559971+sinclairetype0@users.noreply.github.com>
2018-06-07 00:09:19 +0700

    solve tower of hanoi using recursive example

---
+++ Strategy Algo TA/src/strategy/algo/ta/StrategyAlgoTA.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package strategy.algo.ta;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Raynaldi Susanto
+ */
+ class TowersOfHanoi {
+
+   public void solve(int n, String start, String auxiliary, String end) {
+       if (n == 1) {
+           System.out.println(start + " -> " + end);
+       } else {
+           solve(n - 1, start, end, auxiliary);
+           System.out.println(start + " -> " + end);
+           solve(n - 1, auxiliary, start, end);
+          
+       }
+   }
+ }
+public class StrategyAlgoTA {
+
+    /**
+     * @param args the command line arguments
+     */
+
+
+   public static void main(String[] args) {
+       TowersOfHanoi towersOfHanoi = new TowersOfHanoi();
+       System.out.print("Enter number of discs: ");
+       Scanner scanner = new Scanner(System.in);
+       int discs = scanner.nextInt();
+       towersOfHanoi.solve(discs, "A", "B", "C");
+   }
+}
