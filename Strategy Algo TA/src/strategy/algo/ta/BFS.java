e8134626c4fb1d0e3c3dfb76f9b1d88f83133f57
Bima Indra <bimaindra444@gmail.com>
2018-06-27 22:55:05 +0700

    Tambah constructor dan node tetangga pada file bfs
    Tambah constructor dan node tetangga pada file bfs

---
+++ Strategy Algo TA/src/strategy/algo/ta/BFS.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package strategy.algo.ta;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ *
+ * @author bim
+ */
+public class BFS {
+    private Queue<Node> q;
+    static ArrayList<Node> nodes = new ArrayList<Node>();
+    
+    static class Node {
+        int data;
+        boolean kunjung;
+        List<Node> tetangga;
+        
+        // Constructor
+        Node(int data) {
+            this.data = data;
+            this.tetangga = new ArrayList<>();
+        }
+        
+        // Tambah tetangga
+        public void tbhTetangga(Node nodeTetangga) {
+            this.tetangga.add(nodeTetangga);
+        }
+        
+        // Getter tetangga
+        public List<Node> getTetangga() {
+            return tetangga;
+        }
+        
+        // Setter tetangga
+        public void setTetangga(List<Node> tetangga) {
+            this.tetangga = tetangga;
+        }
+    }
+}
