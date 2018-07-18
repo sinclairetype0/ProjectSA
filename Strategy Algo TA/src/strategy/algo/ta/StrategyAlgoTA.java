d74400fb81eba0d3a7c8d1a2ca093d813739d4f2
Raynaldi Susanto <39559971+sinclairetype0@users.noreply.github.com>
2018-07-18 14:10:50 +0700

    Update tower hanoinya

--- Strategy Algo TA/src/strategy/algo/ta/StrategyAlgoTA.java
+++ Strategy Algo TA/src/strategy/algo/ta/StrategyAlgoTA.java
@@ -14,24 +14,21 @@ import java.util.Scanner;
 
 public class StrategyAlgoTA {
 
-    /**
-     * @param args the command line arguments
-     */
-    static void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod) {
-        if(n == 1) {
-            System.out.println("Move disk 1 from rod " + from_rod + " to rod " + to_rod);
-            return;
-        }
-        
-        towerOfHanoi(n-1, from_rod, aux_rod, to_rod);
-        System.out.println("Move disk " + n + " from rod " + from_rod + " to rod " + to_rod);
-        towerOfHanoi(n-1, aux_rod, to_rod, from_rod);
-    }
+       public void solve(int n, String start, String auxiliary, String end) {
+           if (n == 1) {
+               System.out.println("move disk : " + n + " from " +start + " to " + end);
+           } else {
+               solve(n - 1, start, end, auxiliary);
+               System.out.println("move disk : " + n + " from " +start + " to " + end);
+               solve(n - 1, auxiliary, start, end);
+           }
+       }
 
-   public static void main(String args[]) {
-        Scanner input = new Scanner (System.in);
-        System.out.print("Masukkan jumlah disk : ");
-        int disk = input.nextInt();
-        towerOfHanoi(disk, 'A', 'B', 'C'); 
-    }
+       public static void main(String[] args) {
+           StrategyAlgoTA towersOfHanoi = new StrategyAlgoTA();
+           System.out.print("Enter number of discs: ");
+           Scanner scanner = new Scanner(System.in);
+           int discs = scanner.nextInt();
+           towersOfHanoi.solve(discs, "A", "B", "C");
+       }
 }
